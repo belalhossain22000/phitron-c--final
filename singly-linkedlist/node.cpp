9bec38ba5b2ebb06c0aa5c2a9206e9eb29e4f3ac
belalhossain22000 <belalhossain22000@gmail.com>
2025-07-30 03:14:02 +0600

    created module 6 linkedlist

---
+++ singly-linkedlist/node.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+
+    Node(int val) {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+
+int main()
+{
+    Node* head = new Node(10);
+    Node* a = new Node(10);
+    Node* b =new Node(10) ;
+    Node* c = new Node(10);
+    Node* d= new Node(10);
+   
+   return 0;
+}
