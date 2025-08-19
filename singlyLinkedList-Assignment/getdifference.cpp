32fe27ded1f917584c2dc787d52f1b7d01386fcd
belalhossain22000 <belalhossain22000@gmail.com>
2025-08-20 02:27:28 +0600

    solve 1st problem

---
+++ singlyLinkedList-Assignment/getdifference.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void print_linked_list(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+int max_min_difference(Node *head)
+{
+    if (head == NULL)
+        return 0;
+
+    int maxVal = head->val;
+    int minVal = head->val;
+
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->val > maxVal)
+            maxVal = temp->val;
+        if (temp->val < minVal)
+            minVal = temp->val;
+        temp = temp->next;
+    }
+
+    return maxVal - minVal;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+            break;
+
+        insert_at_tail(head, tail, val);
+    }
+
+    int diff = max_min_difference(head);
+    cout << diff << endl;
+
+    return 0;
+}
